615039caeb50e18522876c6fa008afd367033bab
Tony Chao <tchao@mail.usf.edu>
2021-12-27 17:25:48 -0500

    Change the unnecessary folder structure of product-list/product-list to product-list

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './components/product-list/product-list/product-list.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ShortenPipe } from './pipe/shorten.pipe';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ShortenPipe } from './pipe/shorten.pipe';
     ProductListComponent,
     HeaderComponent,
     SidebarComponent,
-    ShortenPipe
+    ShortenPipe,
+    ProductCategoryMenuComponent
   ],
   imports: [
     BrowserModule,
