9eb5bb9f34cc465d7160fe962c2a59567f6704c8
Tony Chao <tchao@mail.usf.edu>
2021-12-27 13:50:20 -0500

    Created a query method provided by Spring to search for all products matching the category id

--- spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/dao/ProductRepository.java
@@ -1,10 +1,18 @@
 package com.springboot.ecommerce.dao;
 
 import com.springboot.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    /*
+    * Built in Query method (findByCategoryId) to find all products that matches the Category Id
+    * Behind the scenes, Spring will execute a query similar to SELECT * FROM product where category_id = ?
+    * Spring Data REST automatically expose endpoint: http://localhost:8080/api/products/search/findByCategoryId?id=1
+    */
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
