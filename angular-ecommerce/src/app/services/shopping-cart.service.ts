3785899e69ef5ba010418b52e3177fe700f4f2ad
Tony Chao <tchao@mail.usf.edu>
2021-12-31 12:45:52 -0500

    Added a new shopping cart service to add shopping cart items to the shopping cart and compute the total price and quantity of items

---
+++ angular-ecommerce/src/app/services/shopping-cart.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { ShoppingCartItem } from '../common/shopping-cart-item';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ShoppingCartService {
+  shoppingCartItems: ShoppingCartItem[] = [];
+
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+  addItemToShoppingCart(shoppingCartItem: ShoppingCartItem) {
+    // Check if we already have the item in the shopping cart
+    let itemExistsInCart: boolean = false;
+    let existingShoppingCartItem: ShoppingCartItem = null as any;
+
+    if (this.shoppingCartItems.length > 0) {
+      // Find the item in the shopping cart based on item i
+      for (let currentShoppingCartItem of this.shoppingCartItems) {
+        if (currentShoppingCartItem.id === shoppingCartItem.id) {
+          existingShoppingCartItem = currentShoppingCartItem;
+          break;
+        }
+      }
+
+      // Check if we found the item
+      itemExistsInCart = existingShoppingCartItem != null;
+    }
+
+    if (itemExistsInCart) {
+      // Increment the quantity of the item by one
+      existingShoppingCartItem.quantity++;
+    } else {
+      // Otherwise just add the new item to the shopping cart
+      this.shoppingCartItems.push(shoppingCartItem);
+    }
+
+    // Compute shopping cart total price and total quantity
+    this.computeShoppingCartTotals();
+  }
+
+  private computeShoppingCartTotals() {
+    let totalItemPrices: number = 0;
+    let totalItemQuantity: number = 0;
+
+    for (let currentShoppingCartItem of this.shoppingCartItems) {
+      totalItemPrices +=
+        currentShoppingCartItem.quantity * currentShoppingCartItem.unitPrice;
+      totalItemQuantity += currentShoppingCartItem.quantity;
+    }
+
+    // Publish totalPrice and totalQuantity events, so all subscribers who subscribe will receive the new data
+    this.totalPrice.next(totalItemPrices);
+    this.totalQuantity.next(totalItemQuantity);
+  }
+}
