ed080ee8bca036c390160af1f5b5687e1a6f0eaa
Tony Chao <tchao@mail.usf.edu>
2021-12-31 13:45:29 -0500

    Added a click event for the add button to create a new shoppingCartItem and store the newly created item in a shoppingCartItem array within the shopping cart service from product details component

--- angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
+import { ShoppingCartItem } from 'src/app/common/shopping-cart-item';
 import { ProductService } from 'src/app/services/product.service';
+import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,6 +14,7 @@ export class ProductDetailsComponent implements OnInit {
   product!: Product;
 
   constructor(private productService: ProductService,
+    private shoppingCartService: ShoppingCartService,
     private route: ActivatedRoute) {
 
   }
@@ -28,5 +31,12 @@ export class ProductDetailsComponent implements OnInit {
     this.productService.fetchProductByProductId(productId).subscribe((responseData) =>
       this.product = responseData
     );
+  }
+
+  onAddProductToShoppingCart() {
+    console.log(`Adding item to shopping cart: ${this.product.name} | ${this.product.unitPrice}`);
+
+    const shoppingCartItem = new ShoppingCartItem(this.product);
+    this.shoppingCartService.addItemToShoppingCart(shoppingCartItem);
   }
 }
