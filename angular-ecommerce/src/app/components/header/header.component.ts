8cfdadfb639c074526d7d15dc06bdc9e3d80e6b4
Tony Chao <tchao@mail.usf.edu>
2021-12-31 12:53:10 -0500

    Subscribe to the shopping cart service's totalQuantity to receive the data and display newly received data

--- angular-ecommerce/src/app/components/header/header.component.ts
+++ angular-ecommerce/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
 
 @Component({
   selector: 'app-header',
@@ -7,15 +8,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
 
-  constructor(private router: Router) {
+  constructor(private router: Router,
+    private shoppingCartService: ShoppingCartService) {
 
   }
 
   ngOnInit(): void {
+    this.updateShoppingCartStatus();
   }
 
   onSearchProducts(keyword: string) {
     this.router.navigateByUrl(`search/${keyword}`);
+  }
+
+  updateShoppingCartStatus() {
+    this.shoppingCartService.totalQuantity.subscribe(
+      responseData => this.totalQuantity = responseData
+    )
   }
 }
