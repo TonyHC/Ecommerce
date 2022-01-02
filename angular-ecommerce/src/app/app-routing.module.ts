6062be3630dec6b667e63bb433d930a9f05c1c3a
Tony Chao <tchao@mail.usf.edu>
2022-01-02 10:05:53 -0500

    Added new route for checkout component

--- angular-ecommerce/src/app/app-routing.module.ts
+++ angular-ecommerce/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ShoppingCartDetailsComponent } from './components/shopping-cart-details/shopping-cart-details.component';
 
 const routes: Routes = [
+  {path: 'checkout', component: CheckoutComponent},
   {path: 'shopping-cart', component: ShoppingCartDetailsComponent},
   {path: 'products/:id', component: ProductDetailsComponent},
   {path: 'search/:keyword', component: ProductListComponent},
