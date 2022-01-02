682f37fc4ea0d1aa874a49ff7c1f0d4dc27062e9
Tony Chao <tchao@mail.usf.edu>
2022-01-02 10:10:19 -0500

    Added a button that routes the user to the checkout route

--- angular-ecommerce/src/app/components/shopping-cart-details/shopping-cart-details.component.ts
+++ angular-ecommerce/src/app/components/shopping-cart-details/shopping-cart-details.component.ts
@@ -28,6 +28,8 @@ export class ShoppingCartDetailsComponent implements OnInit {
     this.shoppingCartService.totalQuantity.subscribe(
       (responseData) => (this.totalQuantity = responseData)
     );
+
+    this.shoppingCartService.computeShoppingCartTotals();
   }
 
   onIncrementItemQuantity(shoppingCartItem: ShoppingCartItem) {
