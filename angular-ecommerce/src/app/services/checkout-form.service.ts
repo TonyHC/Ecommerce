d219962707e857559d2329a96b5ecf2b835886d2
Tony Chao <tchao@mail.usf.edu>
2022-01-02 19:00:29 -0500

    Created checkout form service with two initial methods to return all months in numeric form and the next 10 years

---
+++ angular-ecommerce/src/app/services/checkout-form.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CheckoutFormService {
+
+  constructor() { }
+
+  getCreditCardMonths(): number[] {
+    let months: number[] = [];
+
+    for(let currentMonth = 1; currentMonth <= 12; currentMonth++) {
+      months.push(currentMonth);
+    }
+
+    return months;
+  }
+
+  getCreditCardYears(): Observable<number[]> {
+    let years: number[] = [];
+
+    const startYear: number = new Date().getFullYear();
+    const endYear: number = startYear + 10;
+
+    for (let currentYear = startYear; currentYear <= endYear; currentYear++) {
+      years.push(currentYear);
+    }
+
+    return of(years);
+  }
+}
