4455edd72bc0861afb218f180d88b6e2704099d4
Tony Chao <tchao@mail.usf.edu>
2022-01-03 13:34:50 -0500

    Added two new methods to retrieve a list of countries and states from spring rest api backend

--- angular-ecommerce/src/app/services/checkout-form.service.ts
+++ angular-ecommerce/src/app/services/checkout-form.service.ts
@@ -1,12 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Country } from '../common/country';
+import { State } from '../common/state';
+
+export interface countriesResponse {
+  _embedded: {
+    countries: Country[];
+  }
+}
+
+export interface statesResponse {
+  _embedded: {
+    states: State[];
+  }
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutFormService {
+  private countriesUrl = 'http://localhost:8080/api/countries';
+  private statesUrl = 'http://localhost:8080/api/states';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCountries(): Observable<Country[]> {
+    return this.http.get<countriesResponse>(this.countriesUrl).pipe(
+      map(responseData => responseData._embedded.countries)
+    );
+  }
+
+  getStates(countryCode: string): Observable<State[]> {
+    const searchStatesUrl = `${this.statesUrl}/search/findByCountryCode?code=${countryCode}`;
+
+    return this.http.get<statesResponse>(searchStatesUrl).pipe(
+      map(responseData => responseData._embedded.states)
+    );
+  }
 
   getCreditCardMonths(startMonth: number): Observable<number[]> {
     let months: number[] = [];
