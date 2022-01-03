af2a3812facb9028373e1a48f51e24b0d6e4941c
Tony Chao <tchao@mail.usf.edu>
2022-01-03 13:27:06 -0500

    Added entity classes for Country and State

---
+++ spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/entity/State.java
@@ -0,0 +1,24 @@
+package com.springboot.ecommerce.entity;
+
+import lombok.Data;
+import net.minidev.json.annotate.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "state")
+@Data
+public class State {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    @JsonIgnore
+    private Country country;
+}
