80af5399bd31d52d03ebcee028ec20bbe52ca9e8
Tony Chao <tchao@mail.usf.edu>
2022-01-05 16:00:00 -0500

    Added a new method to determine whether the credit card number input is deemed valid using luhn's algorithm. Before luhn's algorithm is applied, a simple check is applied to determine if credit card number contains only numbers, dashes or spaces

--- angular-ecommerce/src/app/services/checkout-form.service.ts
+++ angular-ecommerce/src/app/services/checkout-form.service.ts
@@ -62,4 +62,28 @@ export class CheckoutFormService {
 
     return of(years);
   }
+
+  validCreditCardNumber(cardNumber: string) {
+    // Only allow numbers, dash or space
+    if (/[^0-9-\s]+/.test(cardNumber))
+      return false;
+
+    // The Luhn Algorithm
+    var nCheck = 0, nDigit = 0, bEven = false;
+    cardNumber = cardNumber.replace(/\D/g, "");
+
+    for (var n = cardNumber.length - 1; n >= 0; n--) {
+      var cDigit = cardNumber.charAt(n),
+      nDigit = parseInt(cDigit, 10);
+
+      if (bEven) {
+        if ((nDigit *= 2) > 9) nDigit -= 9;
+      }
+
+        nCheck += nDigit;
+        bEven = !bEven;
+      }
+
+      return (nCheck % 10) == 0;
+  }
 }
