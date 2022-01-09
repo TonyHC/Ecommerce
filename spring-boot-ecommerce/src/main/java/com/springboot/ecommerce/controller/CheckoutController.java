0d4d2805514ef0f64388fa0f0d4bf3eea3eba603
Tony Chao <tchao@mail.usf.edu>
2022-01-08 23:44:37 -0500

    Add documentation to checkout rest controller and its post mapping

--- spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/controller/CheckoutController.java
+++ spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/controller/CheckoutController.java
@@ -3,11 +3,18 @@ package com.springboot.ecommerce.controller;
 import com.springboot.ecommerce.dto.Purchase;
 import com.springboot.ecommerce.dto.PurchaseResponse;
 import com.springboot.ecommerce.service.CheckoutService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/checkout")
+@Api(
+        tags = "Checkout"
+)
 public class CheckoutController {
     private final CheckoutService checkoutService;
 
@@ -15,8 +22,9 @@ public class CheckoutController {
         this.checkoutService = checkoutService;
     }
 
+    @ApiOperation(value = "Place a order")
     @PostMapping("/purchase")
-    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+    public PurchaseResponse placeOrder(@Valid @RequestBody Purchase purchase) {
         return this.checkoutService.placeOrder(purchase);
     }
 }
