15a46373b40b4c6a45caade495f2baa392655a15
Tony Chao <tchao@mail.usf.edu>
2022-01-08 23:43:25 -0500

    Add documentation to purchase and purchaseResponse dto

--- spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/dto/Purchase.java
+++ spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/dto/Purchase.java
@@ -4,15 +4,25 @@ import com.springboot.ecommerce.entity.Address;
 import com.springboot.ecommerce.entity.Customer;
 import com.springboot.ecommerce.entity.Order;
 import com.springboot.ecommerce.entity.OrderItem;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.util.Set;
 
 @Data
 public class Purchase {
+    @Schema(required = true)
     private Customer customer;
+
+    @Schema(required = true)
     private Address shippingAddress;
+
+    @Schema(required = true)
     private Address billingAddress;
+
+    @Schema(required = true)
     private Order order;
+
+    @Schema(required = true)
     private Set<OrderItem> orderItems;
 }
--- spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/dto/PurchaseResponse.java
+++ spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/dto/PurchaseResponse.java
@@ -1,8 +1,10 @@
 package com.springboot.ecommerce.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class PurchaseResponse {
+    @Schema(description = "Order tracking number based on uuid", example = "3df735d0-f345-4ea9-ae50-30286376b547")
     private final String orderTrackingNumber;
 }
