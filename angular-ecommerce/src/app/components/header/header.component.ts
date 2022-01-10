ef68d29cdf18657abbc40525cb5b580ba9cb920a
Tony Chao <tchao@mail.usf.edu>
2022-01-09 22:55:29 -0500

    Subscribe to fetchProducts method and add a operator function to ngbTypeahead property bind
    Subscribing to fetchProducts method from product service allows us to extract the product name for each product and store as a string array.
    
    The search operator function checks if search bar input is empty or not. If empty return a empty array. Otherwise, return the first ten matching results from productNames array.
    
    The result of search operator function is used by the ngbTypeahead property bind on search bar input to display a dropdown menu of matching results.

--- angular-ecommerce/src/app/components/header/header.component.ts
+++ angular-ecommerce/src/app/components/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable, OperatorFunction } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, } from 'rxjs/operators';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
 
 @Component({
@@ -10,13 +14,17 @@ import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
 export class HeaderComponent implements OnInit {
   totalQuantity: number = 0;
 
+  productNames: string[] = [];
+
   constructor(private router: Router,
-    private shoppingCartService: ShoppingCartService) {
+    private shoppingCartService: ShoppingCartService,
+    private productService: ProductService) {
 
   }
 
   ngOnInit(): void {
     this.updateShoppingCartStatus();
+    this.initSearchBarTypehead();
   }
 
   onSearchProducts(keyword: string) {
@@ -26,6 +34,20 @@ export class HeaderComponent implements OnInit {
   updateShoppingCartStatus() {
     this.shoppingCartService.totalQuantity.subscribe(
       responseData => this.totalQuantity = responseData
-    )
+    );
   }
+
+  initSearchBarTypehead() {
+    this.productService.fetchProducts().subscribe(
+      responseData => responseData.forEach(product => this.productNames.push(product.name))
+    );
+  }
+
+  search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map(term => term === '' ? []
+        : this.productNames.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+    );
 }
