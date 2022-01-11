7718873f507f64192fc487a6b559c00a678e5dcc
Tony Chao <tchao@mail.usf.edu>
2022-01-11 13:13:20 -0500

    Add a login component to utilize the okta sign in widget
    Inside the login component, we set the okta sign in widget with required data to render the sign in widget, handle login redirect if login succeed or throw an error if login fails.

---
+++ angular-ecommerce/src/app/components/login/login.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { OktaAuth, Tokens } from '@okta/okta-auth-js';
+import * as OktaSignIn from '@okta/okta-signin-widget';
+import AppConfig from '../../app.config';
+
+const DEFAULT_ORIGINAL_URI = window.location.origin;
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit, OnDestroy {
+  signIn: any;
+
+  constructor(private oktaAuth: OktaAuth) {
+    this.signIn = new OktaSignIn({
+      baseUrl: AppConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: AppConfig.oidc.clientId,
+      redirectUri: AppConfig.oidc.redirectUri,
+      authClient: oktaAuth
+    });
+  }
+
+  ngOnInit(): void {
+    // When navigating to a protected route, the route path will be saved as the `originalUri`
+    // If no `originalUri` has been saved, then redirect back to the app root
+    const originalUri = this.oktaAuth.getOriginalUri();
+    if (!originalUri || originalUri === DEFAULT_ORIGINAL_URI) {
+      this.oktaAuth.setOriginalUri('/');
+    }
+
+    this.signIn.showSignInToGetTokens({
+      el: '#okta-sign-in-widget',
+      scopes: AppConfig.oidc.scopes
+    }).then((tokens: Tokens) => {
+      // Remove the widget
+      this.signIn.remove();
+
+      // In this flow the redirect to Okta occurs in a hidden iframe
+      this.oktaAuth.handleLoginRedirect(tokens);
+    }).catch((err: any) => {
+      // Typically due to misconfiguration
+      throw err;
+    });
+  }
+
+  ngOnDestroy(): void {
+      this.signIn.remove();
+  }
+}
