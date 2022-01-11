2fb1a544ef31f3b8ac5840875e3bd137e593729f
Tony Chao <tchao@mail.usf.edu>
2022-01-10 22:56:21 -0500

    Add two new routes for login and okta callback component
    The OktaCallbackComponent handles parsing the response and storing the OAuth2 and OpenID Connect tokens, once the user is authenticated and redirected

--- angular-ecommerce/src/app/app-routing.module.ts
+++ angular-ecommerce/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OktaCallbackComponent } from '@okta/okta-angular';
 import { CheckoutComponent } from './components/checkout/checkout.component';
+import { LoginComponent } from './components/login/login.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ShoppingCartDetailsComponent } from './components/shopping-cart-details/shopping-cart-details.component';
 
 const routes: Routes = [
+  {path: 'login/callback', component: OktaCallbackComponent},
+  {path: 'login', component: LoginComponent},
   {path: 'checkout', component: CheckoutComponent},
   {path: 'shopping-cart', component: ShoppingCartDetailsComponent},
   {path: 'products/:id', component: ProductDetailsComponent},
