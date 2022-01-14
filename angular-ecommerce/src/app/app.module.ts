41dddbfe3cb0bcca7e95e29fe6ddf9764082a858
Tony Chao <tchao@mail.usf.edu>
2022-01-14 16:43:55 -0500

    Remove the product category menu component
    Move the entire feature and functionality of product category menu component to the sidebar component.

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ShortenPipe } from './pipe/shorten.pipe';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { ShoppingCartDetailsComponent } from './components/shopping-cart-details/shopping-cart-details.component';
@@ -28,7 +27,6 @@ import { AccountInfoComponent } from './components/account-info/account-info.com
     HeaderComponent,
     SidebarComponent,
     ShortenPipe,
-    ProductCategoryMenuComponent,
     ProductDetailsComponent,
     ShoppingCartDetailsComponent,
     CheckoutComponent,
