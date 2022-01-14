9b199d6c206b4e9fb5943a8be7cafd56f6a13326
Tony Chao <tchao@mail.usf.edu>
2022-01-14 16:58:16 -0500

    Update the ui and functionality of sidebar component
    Move the footer section from main content (router-outlet) to the bottom of sidebar.
    
    Also, changed and moved the design and functionality of product category menu component to sidebar component.
    
    The navigation of sidebar expect the footer section now uses list group.

--- angular-ecommerce/src/app/components/sidebar/sidebar.component.ts
+++ angular-ecommerce/src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ProductCategory } from 'src/app/common/product-category';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  productCategories!: ProductCategory[];
+  productSubscription!: Subscription;
 
-  constructor() { }
+  constructor(private productService: ProductService) {
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    this.listProductCategories();
+  }
+
+  listProductCategories() {
+    this.productSubscription = this.productService.fetchProductCategories().subscribe(
+      responseData => {
+        console.log(JSON.stringify(responseData));
+        this.productCategories = responseData
+      }
+    );
+  }
 }
