47269f8af3c0f72d71ede112dd8fc9b0aef866ca
Tony Chao <tchao@mail.usf.edu>
2022-01-16 23:35:38 -0500

    Add a spring security config to secure the orders rest api endpoints using oauth2 and jwt
    Previously, orders endpoints were exposed, containing sensitive information that anyone knowing the uri could access freely.

---
+++ spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/config/SpringSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.springboot.ecommerce.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // Protect endpoint /api/orders
+        http.authorizeRequests()
+                .antMatchers("/api/orders/**")
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        // Add cors filter
+        http.cors();
+
+        // Force a non-empty response body for 401 error
+        Okta.configureResourceServer401ResponseBody(http);
+    }
+}
