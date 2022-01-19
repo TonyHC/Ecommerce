5cc042580c8e20e2e55618836bf7c07502d2fa3b
Tony Chao <tchao@mail.usf.edu>
2022-01-18 21:00:42 -0500

    Update the redirectUri to use https instead of http from Okta OpenID Connect config file

--- angular-ecommerce/src/app/app.config.ts
+++ angular-ecommerce/src/app/app.config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oa3jv8ae2U8XTDxg5d7',
     issuer: 'https://dev-42917626.okta.com/oauth2/default ',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email']
   }
 }
