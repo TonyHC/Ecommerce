295669c62327aba81a92ae6529078607624ce479
Tony Chao <tchao@mail.usf.edu>
2022-01-21 23:19:29 -0500

    Update the order history and item model class to include additional data for updated order history ui

--- angular-ecommerce/src/app/common/order-history.ts
+++ angular-ecommerce/src/app/common/order-history.ts
@@ -1,7 +1,12 @@
+import { Customer } from "./customer";
+import { OrderItem } from "./order-item";
+
 export class OrderHistory {
   id!: number;
   orderTrackingNumber!: string;
   totalPrice!: number;
   totalQuantity!: number;
   dateCreated!: Date;
+  customer!: Customer;
+  orderItems!: OrderItem[];
 }
--- angular-ecommerce/src/app/common/order-item.ts
+++ angular-ecommerce/src/app/common/order-item.ts
@@ -1,12 +1,14 @@
 import { ShoppingCartItem } from "./shopping-cart-item";
 
 export class OrderItem {
+  name: string;
   imageUrl: string;
   unitPrice: number;
   quantity: number;
   productId: string;
 
   constructor(shoppingCartItem: ShoppingCartItem) {
+    this.name = shoppingCartItem.name;
     this.imageUrl = shoppingCartItem.imageUrl;
     this.unitPrice = shoppingCartItem.unitPrice;
     this.quantity = shoppingCartItem.quantity;
