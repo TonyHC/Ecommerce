ac017461a5ad0d542998f1d23879d847ead48c0d
Tony Chao <tchao@mail.usf.edu>
2022-08-29 22:53:17 -0400

    Update the SQL script to fix some SQL syntax errors

--- database/initdb.sql
+++ database/initdb.sql
@@ -1,125 +1,123 @@
 -- -----------------------------------------------------
 -- Schema ecommerce
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS ecommerce;
-
-CREATE SCHEMA ecommerce;
-USE ecommerce;
+CREATE SCHEMA eCommerce;
+USE eCommerce;
 
 -- -----------------------------------------------------
 -- Table ecommerce.product_category
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS product_category (
-  id BIGINT(20) NOT NULL AUTO_INCREMENT,
-  category_name VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (id)
-) AUTO_INCREMENT = 1;
+CREATE TABLE product_category (
+    id BIGINT(20) NOT NULL AUTO_INCREMENT,
+    category_name VARCHAR(255) NULL DEFAULT NULL,
+    PRIMARY KEY (id)
+) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
 -- Table ecommerce.product
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS product (
-  id BIGINT(20) NOT NULL AUTO_INCREMENT,
-  sku VARCHAR(255) DEFAULT NULL,
-  name VARCHAR(255) DEFAULT NULL,
-  description VARCHAR(255) DEFAULT NULL,
-  unit_price DECIMAL(13,2) DEFAULT NULL,
-  image_url VARCHAR(255) DEFAULT NULL,
-  active BIT DEFAULT 1,
-  units_in_stock INT(11) DEFAULT NULL,
-   date_created DATETIME(6) DEFAULT NULL,
-  last_updated DATETIME(6) DEFAULT NULL,
-  category_id BIGINT(20) NOT NULL,
-  PRIMARY KEY (id),
-  KEY fk_category (category_id),
-  CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES product_category (id)
-) AUTO_INCREMENT = 1;
+CREATE TABLE product (
+    id BIGINT(20) NOT NULL AUTO_INCREMENT,
+    sku VARCHAR(255) DEFAULT NULL,
+    name VARCHAR(255) DEFAULT NULL,
+    description VARCHAR(255) DEFAULT NULL,
+    unit_price DECIMAL(13,2) DEFAULT NULL,
+    image_url VARCHAR(255) DEFAULT NULL,
+    active BIT DEFAULT 1,
+    units_in_stock INT(11) DEFAULT NULL,
+    date_created DATETIME(6) DEFAULT NULL,
+    last_updated DATETIME(6) DEFAULT NULL,
+    category_id BIGINT(20) NOT NULL,
+    PRIMARY KEY (id),
+    KEY fk_category (category_id),
+    CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES product_category (id)
+) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
 -- Table ecommerce.country
 -- -----------------------------------------------------
 CREATE TABLE country (
-  id smallint unsigned NOT NULL AUTO_INCREMENT,
-  code varchar(2) DEFAULT NULL,
-  name varchar(255) DEFAULT NULL,
-  PRIMARY KEY (id)
-) AUTO_INCREMENT=1;
+     id SMALLINT unsigned NOT NULL AUTO_INCREMENT,
+     code VARCHAR(2) DEFAULT NULL,
+     name VARCHAR(255) DEFAULT NULL,
+     PRIMARY KEY (id)
+) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
 -- Table ecommerce.state
 -- -----------------------------------------------------
 CREATE TABLE state (
-  id smallint unsigned NOT NULL AUTO_INCREMENT,
-  name varchar(255) DEFAULT NULL,
-  country_id smallint unsigned NOT NULL,
-  PRIMARY KEY (id),
-  KEY fk_country (country_id),
-  CONSTRAINT fk_country FOREIGN KEY (country_id) REFERENCES country (id)
-) AUTO_INCREMENT=1;
+    id SMALLINT unsigned NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) DEFAULT NULL,
+    country_id smallint unsigned NOT NULL,
+    PRIMARY KEY (id),
+    KEY fk_country (country_id),
+    CONSTRAINT fk_country FOREIGN KEY (country_id) REFERENCES country (id)
+) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
 -- Table ecommerce.address
 -- -----------------------------------------------------
 CREATE TABLE address (
-  id bigint NOT NULL AUTO_INCREMENT,
-  city varchar(255) DEFAULT NULL,
-  country varchar(255) DEFAULT NULL,
-  state varchar(255) DEFAULT NULL,
-  street varchar(255) DEFAULT NULL,
-  zip_code varchar(255) DEFAULT NULL,
-  PRIMARY KEY (id)
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    city VARCHAR(255) DEFAULT NULL,
+    country VARCHAR(255) DEFAULT NULL,
+    state VARCHAR(255) DEFAULT NULL,
+    street VARCHAR(255) DEFAULT NULL,
+    zip_code VARCHAR(255) DEFAULT NULL,
+    PRIMARY KEY (id)
 ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
 -- Table ecommerce.customer
 -- -----------------------------------------------------
 CREATE TABLE customer (
-  id bigint NOT NULL AUTO_INCREMENT,
-  first_name varchar(255) DEFAULT NULL,
-  last_name varchar(255) DEFAULT NULL,
-  email varchar(255) DEFAULT NULL,
-  PRIMARY KEY (id),
-  UNIQUE (email)
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(255) DEFAULT NULL,
+    last_name VARCHAR(255) DEFAULT NULL,
+    email VARCHAR(255) DEFAULT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (email)
 ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
 -- Table ecommerce.orders
 -- -----------------------------------------------------
 CREATE TABLE orders (
-  id bigint NOT NULL AUTO_INCREMENT,
-  order_tracking_number varchar(255) DEFAULT NULL,
-  total_price decimal(19,2) DEFAULT NULL,
-  total_quantity int DEFAULT NULL,
-  billing_address_id bigint DEFAULT NULL,
-  customer_id bigint DEFAULT NULL,
-  shipping_address_id bigint DEFAULT NULL,
-  status varchar(128) DEFAULT NULL,
-  date_created datetime(6) DEFAULT NULL,
-  last_updated datetime(6) DEFAULT NULL,
-  PRIMARY KEY (id),
-  UNIQUE KEY UK_billing_address_id (billing_address_id),
-  UNIQUE KEY UK_shipping_address_id (shipping_address_id),
-  KEY K_customer_id (customer_id),
-  CONSTRAINT FK_customer_id FOREIGN KEY (customer_id) REFERENCES customer (id),
-  CONSTRAINT FK_billing_address_id FOREIGN KEY (billing_address_id) REFERENCES address (id),
-  CONSTRAINT FK_shipping_address_id FOREIGN KEY (shipping_address_id) REFERENCES address (id)
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    order_tracking_number VARCHAR(255) DEFAULT NULL,
+    total_price DECIMAL(19,2) DEFAULT NULL,
+    total_quantity INT DEFAULT NULL,
+    billing_address_id BIGINT DEFAULT NULL,
+    customer_id BIGINT DEFAULT NULL,
+    shipping_address_id BIGINT DEFAULT NULL,
+    status VARCHAR(128) DEFAULT NULL,
+    date_created DATETIME(6) DEFAULT NULL,
+    last_updated DATETIME(6) DEFAULT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY UK_billing_address_id (billing_address_id),
+    UNIQUE KEY UK_shipping_address_id (shipping_address_id),
+    KEY K_customer_id (customer_id),
+    CONSTRAINT FK_customer_id FOREIGN KEY (customer_id) REFERENCES customer (id),
+    CONSTRAINT FK_billing_address_id FOREIGN KEY (billing_address_id) REFERENCES address (id),
+    CONSTRAINT FK_shipping_address_id FOREIGN KEY (shipping_address_id) REFERENCES address (id)
 ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
 -- Table ecommerce.order_item
 -- -----------------------------------------------------
 CREATE TABLE order_item (
-  id bigint NOT NULL AUTO_INCREMENT,
-  name varchar(255) DEFAULT NULL,
-  image_url varchar(255) DEFAULT NULL,
-  quantity int DEFAULT NULL,
-  unit_price decimal(19,2) DEFAULT NULL,
-  order_id bigint DEFAULT NULL,
-  product_id bigint DEFAULT NULL,
-  PRIMARY KEY (id),
-  KEY K_order_id (order_id),
-  CONSTRAINT FK_order_id FOREIGN KEY (order_id) REFERENCES orders (id),
-  CONSTRAINT FK_product_id FOREIGN KEY (product_id) REFERENCES product (id)
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) DEFAULT NULL,
+    image_url VARCHAR(255) DEFAULT NULL,
+    quantity INT DEFAULT NULL,
+    unit_price decimal(19,2) DEFAULT NULL,
+    order_id BIGINT DEFAULT NULL,
+    product_id BIGINT DEFAULT NULL,
+    PRIMARY KEY (id),
+    KEY K_order_id (order_id),
+    CONSTRAINT FK_order_id FOREIGN KEY (order_id) REFERENCES orders (id),
+    CONSTRAINT FK_product_id FOREIGN KEY (product_id) REFERENCES product (id)
 ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- -----------------------------------------------------
@@ -249,7 +247,7 @@ INSERT INTO product (sku, name, description, image_url, active, units_in_stock, 
 -- -----------------------------------------------------
 -- Populate Country
 -- -----------------------------------------------------
-INSERT INTO country (code, name) VALUES 
+INSERT INTO country (code, name) VALUES
 ('BR','Brazil'),
 ('CA','Canada'),
 ('DE','Germany'),
@@ -260,7 +258,7 @@ INSERT INTO country (code, name) VALUES
 -- -----------------------------------------------------
 -- Populate State
 -- -----------------------------------------------------
-INSERT INTO state (name, country_id) VALUES 
+INSERT INTO state (name, country_id) VALUES
 ('Acre',1),
 ('Alagoas',1),
 ('Amapá',1),
