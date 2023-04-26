80a6140744fb3d057f31af790799503092e6eeec
Tony Chao <tchao@mail.usf.edu>
2023-04-26 15:38:15 -0400

    Merge the configurations from RestControllerConfig to WebRoutingConfig class

--- spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/config/WebRoutingConfig.java
+++ spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/config/WebRoutingConfig.java
@@ -1,8 +1,10 @@
 package com.springboot.ecommerce.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
@@ -11,6 +13,17 @@ import java.io.IOException;
 
 @Configuration
 public class WebRoutingConfig implements WebMvcConfigurer {
+    @Value("${allowed.origins}")
+    private String[] allowedOrigins;
+
+    @Value("${spring.data.rest.base-path}")
+    private String basePath;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping(basePath + "/**").allowedOrigins(allowedOrigins);
+    }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**/*")
